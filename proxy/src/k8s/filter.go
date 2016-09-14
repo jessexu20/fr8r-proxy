9bfb35a979f01299f8b14192d3ed85896dfa9c7e
Tommaso Madonia <tmadonia@us.ibm.com>
2016-09-13 21:15:52 -0400

    Added filters for events
    - Replace source.host
    - Change message when reason is Scheduled or FailedScheduling

--- proxy/src/k8s/filter.go
+++ proxy/src/k8s/filter.go
@@ -3,17 +3,43 @@ package k8s
 import (
 	"encoding/json"
 	"github.com/golang/glog"
+	"regexp"
 	"strings"
 )
 
 type filterObject struct {
 	filterType string
-	path       []string
+	selector   Selector
+	field      string
 	value      interface{}
+	regex      string
 }
 
 type FilterCollection struct {
 	filters map[string][]filterObject
+}
+
+type Selector struct {
+	Path []string
+	Test func(interface{}) bool
+}
+
+func NotNilSelector(path ...string) *Selector {
+	return &Selector{
+		Path: path,
+		Test: func(test interface{}) bool {
+			return test != nil
+		},
+	}
+}
+
+func IsEqualsSelector(equals interface{}, path ...string) *Selector {
+	return &Selector{
+		Path: path,
+		Test: func(test interface{}) bool {
+			return test == equals
+		},
+	}
 }
 
 func NewFilterCollection() *FilterCollection {
@@ -32,20 +58,26 @@ func (collection FilterCollection) addFilter(kind string, filter filterObject) {
 	collection.filters[kind] = filters
 }
 
-func (collection FilterCollection) AddRemoveFilter(kind string, path ...string) {
-	filter := filterObject{filterType: "remove", path: path}
+func (collection FilterCollection) AddRemoveFilter(kind string, selector *Selector, field string) {
+	filter := filterObject{filterType: "remove", selector: *selector, field: field}
 
 	collection.addFilter(kind, filter)
 }
 
-func (collection FilterCollection) AddReplaceFilter(value interface{}, kind string, path ...string) {
-	filter := filterObject{filterType: "replace", path: path, value: value}
+func (collection FilterCollection) AddReplaceFilter(value interface{}, kind string, selector *Selector, field string) {
+	filter := filterObject{filterType: "replace", selector: *selector, value: value, field: field}
 
 	collection.addFilter(kind, filter)
 }
 
-func (collection FilterCollection) AddEmptyFilter(kind string, path ...string) {
-	filter := filterObject{filterType: "replace", path: path, value: ""}
+func (collection FilterCollection) AddEmptyFilter(kind string, selector *Selector, field string) {
+	filter := filterObject{filterType: "replace", selector: *selector, value: "", field: field}
+
+	collection.addFilter(kind, filter)
+}
+
+func (collection FilterCollection) AddRegexFilter(regex string, value string, kind string, selector *Selector, field string) {
+	filter := filterObject{filterType: "regex", selector: *selector, value: value, field: field, regex: regex}
 
 	collection.addFilter(kind, filter)
 }
@@ -142,61 +174,101 @@ func (collection FilterCollection) filter(data map[string]interface{}) bool {
 func filterData(data map[string]interface{}, filters []filterObject) bool {
 	var filtered = false
 	for _, filter := range filters {
-		switch filter.filterType {
-
-		case "remove":
-			if remove(data, filter.path) {
-				filtered = true
-			}
-
-		case "replace":
-			if replace(data, filter.value, filter.path) {
-				filtered = true
-			}
-
+		if filter.apply(data) {
+			filtered = true
 		}
 	}
 
 	return filtered
 }
 
-func remove(data map[string]interface{}, path []string) bool {
-	if len(path) == 0 {
-		return false
-	} else if data[path[0]] == nil {
-		return false
-	} else if len(path) == 1 {
-		delete(data, path[0])
-
-		return true
-	}
-
-	first, ok := data[path[0]].(map[string]interface{})
-	if !ok {
+func (filter filterObject) apply(data map[string]interface{}) bool {
+	if filter.field == "" {
 		return false
 	}
-	return remove(first, path[1:])
+
+	parent := findParent(data, filter.selector)
+	if parent == nil {
+		return false
+	}
+
+	switch filter.filterType {
+
+	case "remove":
+		return remove(parent, filter.field)
+
+	case "replace":
+		return replace(parent, filter.value, filter.field)
+
+	case "regex":
+		return replaceRegex(parent, filter.regex, filter.value, filter.field)
+
+	default:
+		return false
+
+	}
 }
 
-func replace(data map[string]interface{}, value interface{}, path []string) bool {
-	if len(path) == 0 {
-		return false
+func findParent(data map[string]interface{}, selector Selector) map[string]interface{} {
+	if selector.Path == nil || selector.Test == nil || len(selector.Path) == 0 {
+		return nil
 	}
 
-	if len(path) == 1 {
-		if data[path[0]] != nil {
-			data[path[0]] = value
-			return true
+	var object = data
+	for _, pathComponent := range selector.Path[:len(selector.Path)-1] {
+		subObject, ok := object[pathComponent].(map[string]interface{})
+		if !ok {
+			return nil
 		}
 
-		return false
-	} else if data[path[0]] == nil {
+		object = subObject
+	}
+
+	if selector.Test(object[selector.Path[len(selector.Path)-1]]) {
+		return object
+	}
+
+	return nil
+}
+
+func remove(data map[string]interface{}, field string) bool {
+	if data[field] == nil {
 		return false
 	}
 
-	first, ok := data[path[0]].(map[string]interface{})
+	delete(data, field)
+	return true
+}
+
+func replace(data map[string]interface{}, value interface{}, field string) bool {
+	if data[field] == nil || data[field] == value {
+		return false
+	}
+
+	data[field] = value
+	return true
+}
+
+func replaceRegex(data map[string]interface{}, regex string, value interface{}, field string) bool {
+	if data[field] == nil || regex == "" {
+		return false
+	}
+
+	v, ok := value.(string)
 	if !ok {
 		return false
 	}
-	return replace(first, value, path[1:])
+
+	s, ok := data[field].(string)
+	if !ok {
+		return false
+	}
+
+	r, err := regexp.Compile(regex)
+	if err != nil {
+		return false
+	}
+
+	data[field] = r.ReplaceAllString(s, v)
+	return true
 }
